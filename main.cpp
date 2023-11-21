b320f2c56031c25d03c0749872d8b1925cc9bd07
Trind20 <torsrind@icloud.com>
2023-11-21 08:42:09 +0100

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,16 @@
+/** System CLI Menu*/
+#include <iostream>
+#include <string>
+#include <memory>
+
+/** Macro declaration */
+#define Sucsess_Exit_Code 0
+#define Error_Exit_Code 0
+
+
+int main()
+{
+
+
+    return Sucsess_Exit_Code;
+}
