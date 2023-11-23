a8e6b2675bba2168514a32bc1f3e89e1d4c5b275
Trind20 <torsrind@icloud.com>
2023-11-23 08:58:08 +0100

    Refactoring and bugfixes

--- cpp/main_Class.cpp
+++ cpp/main_Class.cpp
@@ -1,29 +1,29 @@
 #pragma once
+#define Sucsess_Exit_Code 0
+#define Error_Exit_Code 1
 
 class Storage
 {
-public:
     /** Storage */
     bool main_loop;
     unsigned int cycles;
     std::string user_Input;
     std::string title = "System-Menu";
 
-    /** Methods */
-
-    void clear();
-
-// public:
+public:
     Storage(bool Loop,int Cycles): main_loop(Loop),cycles(Cycles)
     {
         std::cout << "Loading up.." << std::endl;
         clear();
     }
 
+    int main_Loop();
+    void clear()
+    {system("cls");}
 
     ~Storage()
     {
-        std::cout << "Deleting.." << std::endl;
+        std::cout << "Freeing up allocaded memory.." << std::endl;
         main_loop = 0;
         cycles = 0;
         user_Input = "0";
@@ -31,5 +31,59 @@ public:
     
 };
 
-void Storage::clear()
-{system("cls");}
+int Storage::main_Loop()
+{
+    while(main_loop)
+    {
+        std::cout << "***** SYSTEM-CLI-MENU ****" << std::endl << std::endl;
+        std::cout << "System hardware info: I" << std::endl;
+        std::cout << "System shutdown: S" << std::endl;
+        std::cout << "Cancel system shutdown: C" << std::endl;
+        std::cout << "Terminate: T" << std::endl << std::endl;
+        std::cout << "Field: ";
+        std::cin >> user_Input;
+
+        if(user_Input == "I" || user_Input == "i")
+        {
+            clear();
+            system("systeminfo");
+            system("pause");
+            clear();
+        }
+        else if(user_Input == "S" || user_Input == "s")
+        {
+            clear();
+            std::cout << "Shutdown command executing.. \n\n";
+            system("shutdown -s -t 600");
+            system("pause");
+            clear();
+        }
+
+        else if(user_Input == "C" || user_Input == "c")
+        {
+            clear();
+            std::cout << "Shutdown command executing.. \n\n";
+            system("shutdown -a");
+            system("pause");
+            clear();
+        }
+
+        else if(user_Input == "T" || user_Input == "t")
+        {
+            clear();
+            std::cout << "Terminating software..\n\n";
+
+            return Sucsess_Exit_Code;
+        }
+
+        else
+        {
+            std::cout << "Error invalid input";
+            system("pause");
+            return Error_Exit_Code;
+        }
+
+    }
+    return Sucsess_Exit_Code;
+}
+
--- main.cpp
+++ main.cpp
@@ -3,62 +3,14 @@
 #include <string>
 #include "./header/main_Class.h"
 
-/** Macro declaration */
-#define Sucsess_Exit_Code 0
-#define Error_Exit_Code 1
+
 
 int main()
 {
-    Storage *storage = new Storage(true,0);
+    Storage *Ptr = new Storage(true,0);
 
-    while(storage->main_loop)
-    {
-        std::cout << "***** SYSTEM-CLI-MENU ****" << std::endl << std::endl;
-        std::cout << "System hardware info: I" << std::endl;
-        std::cout << "System shutdown: S" << std::endl;
-        std::cout << "Cancel system shutdown: C" << std::endl;
-        std::cout << "Terminate: T" << std::endl << std::endl;
-        std::cout << "Field: ";
-        std::cin >> storage->user_Input;
+    int ret_value = Ptr->main_Loop();
 
-        if(storage->user_Input == "I" || storage->user_Input == "i")
-        {
-            storage->clear();
-            system("systeminfo");
-        }
-        else if(storage->user_Input == "S" || storage->user_Input == "s")
-        {
-            storage->clear();
-            std::cout << "Shutdown command executing.. ";
-            system("shutdown -s -t 600");
-            system("pause>null");
-        }
-
-        else if(storage->user_Input == "C" || storage->user_Input == "c")
-        {
-            storage->clear();
-            std::cout << "Shutdown command executing.. ";
-            system("shutdown -a");
-            system("pause>null");
-        }
-
-        else if(storage->user_Input == "T" || storage->user_Input == "t")
-        {
-            storage->clear();
-            std::cout << "Terminating software..";
-            delete(storage);
-            return Sucsess_Exit_Code;
-        }
-
-        else
-        {
-            std::cout << "Error invalid input";
-            system("pause>null");
-            delete(storage);
-            return Error_Exit_Code;
-        }
-
-    }
-    delete(storage);
-    return Sucsess_Exit_Code;
+    delete(Ptr);
+    return ret_value;
 }
