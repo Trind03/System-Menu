52f2a344102a7177f9bb1c23b224709a2c00a2e8
Torstein <torsrind@icloud.com>
2024-03-27 07:30:21 +0100

    Refactoring and removing redundant code

--- main.cpp
+++ main.cpp
@@ -1,15 +1,11 @@
 /* Software - by - Torstein */
-#include <iostream>
-#include <string>
-#include "./header/main_Class.h"
+#include <future>
+#include <memory>
+#include "main_Class.cpp"
 #define Version 2.2
 
 int main()
 {
-    Storage *Ptr = new Storage(true,0);
-
-    int ret_value = Ptr->main_Loop();
-
-    delete(Ptr);
-    return ret_value;
+    std::unique_ptr<Storage>storage = std::make_unique<Storage>(true);
+    return storage->main_Loop();
 }
